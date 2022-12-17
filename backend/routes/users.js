bf9903fc83acadb86ff01bf73971c5bbdc3f2a65
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-17 14:48:00 +0000

    feat (add) : authRequired

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,13 +4,15 @@ import {
     getAllUsers,
     getAllAdmins
 } from "../controllers/users.js";
+import { authRequired } from "../utils/jwt.js";
+
 
 const userRoutes = Router();
 
 userRoutes.post("/createUser", createUser);
 
-userRoutes.get("/getAllUsers", getAllUsers);
+userRoutes.get("/getAllUsers", authRequired, getAllUsers);
 
-userRoutes.get("/getAllAdmins", getAllAdmins);
+userRoutes.get("/getAllAdmins", authRequired, getAllAdmins);
 
 export { userRoutes };
