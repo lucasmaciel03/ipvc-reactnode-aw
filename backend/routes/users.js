dba1e074ff0a5dcf49407ccc7e9963fd1e55b9ad
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-17 14:35:56 +0000

    feat (add) : users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,16 @@
 import Router from "express";
 import {
     createUser,
+    getAllUsers,
+    getAllAdmins
 } from "../controllers/users.js";
 
 const userRoutes = Router();
 
 userRoutes.post("/createUser", createUser);
+
+userRoutes.get("/getAllUsers", getAllUsers);
+
+userRoutes.get("/getAllAdmins", getAllAdmins);
 
 export { userRoutes };
