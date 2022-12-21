d56b30f38c3f3682403d107e48f7c8f4fd6424c6
AndreMaia95 <andrefsm95@gmail.com>
2022-12-21 12:06:40 +0000

    feat:header and movie overview

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -5,10 +5,17 @@ const IMG_API = "https://image.tmdb.org/t/p/w500";
 const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
+      <header>
+        <input className="search" type="search" placeholder="Search..." />
+      </header>
       <img src={IMG_API + poster_path} alt={title} />
       <div className="movie-info">
         <h3>{title}</h3>
         <span>{vote_average}</span>
+      </div>
+      <div className="movie-over">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
       </div>
     </div>
   );
