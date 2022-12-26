d8b852eca7cfbb6bcdfb57971f9594d1c821b1e5
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-26 20:26:21 +0000

    feat (add) : Categories

---
+++ frontend/src/components/Client/Categories/Categories.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './categories.css'
+function Categories() {
+  return (
+    // Temos de adicionar uma lista de Categorias na horizontal aqui em baixo
+    
+  )
+}
+
+export default Categories
