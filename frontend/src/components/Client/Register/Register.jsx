da961cc98cdfbd83ed78b880b091de96b1efce7e
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-27 23:25:41 +0000

    feat (add) : connect Register form to backend

--- frontend/src/components/Client/Register/Register.jsx
+++ frontend/src/components/Client/Register/Register.jsx
@@ -1,74 +1,134 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 import "./register.css";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const register = () => {
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, repeatPassword);
+
+    //validate password and repeat password
+    if (password !== repeatPassword) {
+      toast.error('❌ Password erro', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+      return;
+    }
+
+    // call controller to register user
+    fetch("http://localhost:4243/api/users/createUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.status === "success") {
+          toast.success('✅ User created', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            });
+          ;
+        } else {
+          toast.success('✅ User created', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            });
+        }
+      });
+  };
+  
   return (
     <div className="registerform">
-    <section className="login">
-      <div className="btns-login">
-        <h2>Sign Up</h2>
-      </div>
-      <div className="container">
-        <form
-          action="#"
-          className="form-entrar ativo"
-          method="post"
-          id="entrar"
-        >
-          <div className="username-field ct-input">
-            <label htmlFor="usuario-entrar">Username</label>
-            <input
-              type="text"
-              id="usuario-entrar"
-              name="usuario-entrar"
-              placeholder="Username"
-            />
-          </div>
+      <section className="login">
+        <div className="btns-login">
+          <h2>Sign Up</h2>
+        </div>
+        <div className="container">
+          <form
+            action="#"
+            className="form-entrar ativo"
+            method="post"
+            id="entrar"
+            onSubmit={handleSubmit}
+          >
+            <div className="username-field ct-input">
+              <label htmlFor="usuario-entrar">Username</label>
+              <input
+                type="text"
+                id="usuario-entrar"
+                name="usuario-entrar"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
 
-          <div className="username-field ct-input">
-            <label htmlFor="usuario-entrar">Email</label>
-            <input
-              type="email"
-              id="usuario-entrar"
-              name="usuario-entrar"
-              placeholder="Email"
-            />
-          </div>
+            <div className="senha-field ct-input">
+              <label htmlFor="senha-entrar">Password</label>
+              <input
+                type="password"
+                id="senha-entrar"
+                name="senha-entrar"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <div className="senha-field ct-input">
-            <label htmlFor="senha-entrar">Password</label>
-            <input
-              type="password"
-              id="senha-entrar"
-              name="senha-entrar"
-              placeholder="Password"
-            />
-          </div>
-
-          <div className="senha-field ct-input">
-            <label htmlFor="senha-entrar">Repeat Password</label>
-            <input
-              type="password"
-              id="senha-entrar"
-              name="senha-entrar"
-              placeholder="Repeat Password"
-            />
-          </div>
-          <div className="remember-create-link">
-            <Link to="/login" className="create-link">
-              I already have an account.
-            </Link>
-          </div>
-          <button type="submit" className="submit">
-            Register
-          </button>
-        </form>
-      </div>
-    </section>
-    ;
-  </div>
+            <div className="senha-field ct-input">
+              <label htmlFor="senha-entrar">Repeat Password</label>
+              <input
+                type="password"
+                id="senha-entrar"
+                name="senha-entrar"
+                placeholder="Repeat Password"
+                onChange={(e) => setRepeatPassword(e.target.value)}
+              />
+            </div>
+            <div className="remember-create-link">
+              <Link to="/login" className="create-link">
+                I already have an account.
+              </Link>
+            </div>
+            <button type="submit" className="submit">
+              Register
+            </button>
+          </form>
+        </div>
+      </section>
+      ;
+    </div>
   )
 };
 
-export default register;
+export default Register;
