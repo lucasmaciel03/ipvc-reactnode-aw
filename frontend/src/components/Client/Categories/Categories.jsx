3fe23a63eeb408c87d7abd274abd813e08e06364
AndreMaia95 <andrefsm95@gmail.com>
2022-12-27 18:02:32 +0000

    fix:awdaw

--- frontend/src/components/Client/Categories/Categories.jsx
+++ frontend/src/components/Client/Categories/Categories.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./categories.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
 
 function Categories() {
   return <></>;
