6cb796564337d0a5f3721167c0ed6efdbf5a3707
AndreMaia95 <andrefsm95@gmail.com>
2022-12-27 11:46:24 +0000

    fix: card fav button

--- frontend/src/components/Client/Card/Cards.jsx
+++ frontend/src/components/Client/Card/Cards.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./card.css";
+import star100white from "../../../images/star100white.png";
+import star100cor from "../../../images/start100cor.png";
 
 const Cards = () => {
   return (
@@ -27,7 +29,7 @@ const Cards = () => {
         </button>
         <div className="fav">
           <a href="#">
-            <img src="../../../images/star100white.png"></img>
+            <img src={star100white}></img>
           </a>
         </div>
       </div>
