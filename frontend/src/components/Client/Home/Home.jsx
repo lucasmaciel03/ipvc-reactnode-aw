412ddd02db204b95835918a0bcf5ddfb1bfc501c
AndreMaia95 <andrefsm95@gmail.com>
2022-12-28 13:48:26 +0000

    fix: delete categories

--- frontend/src/components/Client/Home/Home.jsx
+++ frontend/src/components/Client/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Cards from "../Card/Cards";
 import "./home.css";
-import Categories from "../Categories/Categories.jsx";
 
 function Home() {
   return (
     <div className="movies">
-      <Categories></Categories>
       <div className="movie-text">Movies</div>
       <Cards></Cards>
       <Cards></Cards>
--- frontend/src/components/Client/Series/Series.jsx
+++ frontend/src/components/Client/Series/Series.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Cards from "../Card/Cards.jsx";
 import "./series.css";
-import Categories from "../Categories/Categories.jsx";
 
 function Series() {
   return (
     <div className="series">
-      <Categories></Categories>
       <div className="series-text">Series</div>
       <Cards></Cards>
       <Cards></Cards>
