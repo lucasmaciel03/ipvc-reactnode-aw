274ec7aabda1b965bb2e2e59c814216674356c50
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-04 15:29:26 +0000

    edit : add new film controller

--- backend/controllers/films.js
+++ backend/controllers/films.js
@@ -1,35 +1,60 @@
-import { FilmModel } from "../models/films.js";
-import { CategoryModel } from "../models/categories.js";
-import fs from "fs";
+import { FilmModel } from '../models/films.js';
+import { CategoryModel } from '../models/categories.js';
+
+import path from 'path';
+import fs from 'fs';
 
 export const createFilm = async (req, res) => {
   try {
-    // Check if film already exists
-    const film = await FilmModel.findOne({ where: { title: req.body.title } });
-    if (film) {
-      return res.status(400).json({ message: "Film already exist" });
+    const {
+      categoryId,
+      name,
+      description,
+      releaseDate,
+      time,
+      image,
+      rentPrice,
+    } = req.body;
+
+    // Verifica se o nome de filme já está sendo utilizado
+    const filmWithSameName = await FilmModel.findOne({ where: { name } });
+    if (filmWithSameName) {
+      return res.status(400).send({ error: 'Film name already in use' });
     }
 
-    // Check if category exists
-    const category = await CategoryModel.findOne({ where: { id: req.body.id_category } });
+    // Verifica se a categoria existe
+    const category = await CategoryModel.findByPk(categoryId);
     if (!category) {
-      return res.status(400).json({ message: "Category not exist" });
+      return res.status(400).send({ error: 'Category not found' });
     }
 
-    // Read image file and store in a variable
-    const imageData = fs.readFileSync(req.file.path);
+    // Verifica se o tempo do filme não é menor ou igual a 0
+    if (time <= 0) {
+      return res.status(400).send({ error: 'Invalid film time' });
+    }
 
-    // Create film with image data
-    const newFilm = await FilmModel.create({
-      title: req.body.title,
-      description: req.body.description,
-      id_category: req.body.id_category,
-      price: req.body.price,
-      duration: req.body.duration,
-      image: imageData,
+    // Verifica se o Rentprice não é menor ou igual a 0
+    if (rentPrice <= 0) {
+      return res.status(400).send({ error: 'Invalid film rent price' });
+    }
+
+    // Guarda a imagem tanto na base de dados como numa pasta de imagens
+    const imagePath = path.join(__dirname, '../public/images', image.name);
+    fs.writeFileSync(imagePath, image.data);
+
+    const film = await FilmModel.create({
+      categoryId,
+      name,
+      description,
+      releaseDate,
+      time,
+      image: image.name,
+      rentPrice,
     });
-    return res.status(201).json({ message: "Film created", newFilm });
+
+    return res.status(201).send({ film });
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).send({ error: error.message });
   }
 };
+
