d84342eb148f6c2e1c65de1d5d88b07932b20d78
AndreMaia95 <andrefsm95@gmail.com>
2023-01-09 13:40:51 +0000

    feat: add information to history rent

--- frontend/src/components/Client/HistoryRent/Historyrent.jsx
+++ frontend/src/components/Client/HistoryRent/Historyrent.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./historyrent.css";
+import Card from "../Card/Cards.jsx";
 
 const Historyrent = () => {
-  return <div>awd</div>;
+  return (
+    <div className="History-rent">
+      <div className="title-history-rent">
+        <h1>There were the movies you have rented</h1>
+      </div>
+      <div className="history-rent">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </div>
+  );
 };
 
 export default Historyrent;
