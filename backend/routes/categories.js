dbe42ea03332cc0c2a2341b467d4888d40236b65
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-10 23:14:11 +0000

    add : new category route

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,6 +2,7 @@ import Router from "express";
 import {
     getAllCategories,
     createCategory,
+    deleteCategory,
 } from "../controllers/categories.js";
 
 const categoryRoutes = Router();
@@ -9,5 +10,7 @@ const categoryRoutes = Router();
 categoryRoutes.get("/getAllCategories", getAllCategories);
 
 categoryRoutes.post("/createCategory", createCategory);
+
+categoryRoutes.delete("/deleteCategory/:id", deleteCategory);
 
 export { categoryRoutes };
