0838364aca173e921b1bf39ff53e321c1cb7763b
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-10 23:03:16 +0000

    feat add : get all categories

--- frontend/src/components/filter/FormCategory.js
+++ frontend/src/components/filter/FormCategory.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
 import WrapperFiltroSection from "../utilities/WrapperFiltroSection";
 import classes from "./FormCategory.module.css";
+import axios from "axios";
 
-const categories = ["flores", "cactos", "outros"];
 
 const FormCategory = ({ setFilter, filterVal }) => {
   const [selectedCategories, setSelectedCategories] = useState(filterVal);
+
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const url = "http://localhost:4243/api/categories/getAllCategories";
+    const res = await axios.get(url);
+
+    if(!res) return;
+    setCategories(res.data);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, [categories]);
+
 
   const handleChange = ({ target }) => {
     if (target.checked) {
@@ -29,16 +44,16 @@ const FormCategory = ({ setFilter, filterVal }) => {
     <WrapperFiltroSection title="Categorias">
       <form className={classes.form}>
         {categories.map((category) => (
-          <div className={classes.divInput} key={category}>
+          <div className={classes.divInput} key={category.id}>
             <input
               type="checkbox"
               name="categoria"
-              id={category}
-              value={category}
+              id={category.name}
+              value={category.name}
               onChange={handleChange}
-              checked={selectedCategories.includes(category)}
+              checked={selectedCategories.includes(category.name)}
             />
-            <label htmlFor={category}>{category}</label>
+            <label htmlFor={category.name}>{category.name}</label>
           </div>
         ))}
       </form>
