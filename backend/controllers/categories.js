54c7f1d6b5f48c20d3a3323bc7bdb5b87ed8c510
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-10 15:17:13 +0000

    add : new controllers

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -8,17 +8,38 @@ export const getAllCategories = async (req, res) => {
     res.json(categories);
 }
 
-// create a new category dont accept number as name
+// create a new category dont accept number as name, verify if the name is already in use
 export const createCategory = async (req, res) => {
     const { name } = req.body;
-    if (isNaN(name)) {
-        const newCategory = await CategoryModel.create({
-            name
-        });
-        res.json(newCategory);
-    } else {
-        res.json('Name must be a string');
+    if (!isNaN(name)) {
+        res.status(400).json({ error: 'Name must be a string' });
+        return;
     }
+    const category = await CategoryModel.findOne({
+        where: {
+            name
+        }
+    });
+    if (category) {
+        res.status(400).json({ error: 'Category already exists' });
+        return;
+    }
+    const newCategory = await CategoryModel.create({
+        name
+    });
+    res.json(newCategory);
+}
+
+// delete a category by id
+export const deleteCategory = async (req, res) => {
+    const { id } = req.params;
+    const category = await CategoryModel.findByPk(id);
+    if (!category) {
+        res.status(404).json({ error: 'Category not found' });
+        return;
+    }
+    await category.destroy();
+    res.json(category);
 }
 
 
