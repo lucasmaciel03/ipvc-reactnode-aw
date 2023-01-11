c65749cc5046001b1ceb28a30172f3f1fe2187e4
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-11 19:28:25 +0000

    add : new alerts

--- frontend/src/components/register/Register.js
+++ frontend/src/components/register/Register.js
@@ -1,28 +1,35 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
 import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
+// import Stack from '@mui/material/Stack';
+// import Snackbar from '@mui/material/Snackbar';
+// import Alert from '@mui/material/Alert';
 import {
-  Backdrop,
-  Button,
-  CircularProgress,
-  TextField,
-  Alert,
   Snackbar,
-} from "@mui/material";
+  Alert
+} from "@mui/material"
+
+
 
 function Register() {
-  <ToastContainer />;
+  const [openToast2, setOpenToast2] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
 
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(eyeOff);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenToast2(false);
+  };
 
   const handleToggle = () => {
     if (type === "password") {
@@ -40,16 +47,8 @@ function Register() {
 
     //validate password and repeat password
     if (password !== repeatPassword) {
-      toast.error("❌ Password erro", {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
+      console.log('----------------------------------------ENTREI------------------------')
+      setOpenToast2(true);
       return;
     }
 
@@ -68,96 +67,84 @@ function Register() {
       .then((data) => {
         console.log(data);
         if (data.status === "success") {
-          toast.success("✅ User created", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
+          alert("✅ User created");
         } else {
-          toast.success("✅ User created", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
+          alert("❌ User not created");
         }
       });
   };
 
   return (
-    <div className="registerform">
-      <section className="login">
-        <div className="btns-login">
-          <h2>Sign Up</h2>
-        </div>
-        <div className="container-lg">
-          <form
-            action="#"
-            className="form-entrar ativo"
-            method="post"
-            id="entrar"
-            onSubmit={handleSubmit}
-          >
-            <div className="username-field ct-input">
-              <label htmlFor="usuario-entrar">Username</label>
-              <input
-                type="text"
-                id="usuario-entrar"
-                name="usuario-entrar"
-                placeholder="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
+    <>
+      <div className="registerform">
+        <section className="login">
+          <div className="btns-login">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="container-lg">
+            <form
+              action="#"
+              className="form-entrar ativo"
+              method="post"
+              id="entrar"
+              onSubmit={handleSubmit}
+            >
+              <div className="username-field ct-input">
+                <label htmlFor="usuario-entrar">Username</label>
+                <input
+                  type="text"
+                  id="usuario-entrar"
+                  name="usuario-entrar"
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
 
-            <div className="senha-field ct-input">
-              <label htmlFor="senha-entrar">Password</label>
-              <span onClick={handleToggle}>
-                <Icon icon={icon} size={20} />
-              </span>
-              <input
-                type={type}
-                id="senha-entrar"
-                name="senha-entrar"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
+              <div className="senha-field ct-input">
+                <label htmlFor="senha-entrar">Password</label>
+                <span onClick={handleToggle}>
+                  <Icon icon={icon} size={20} />
+                </span>
+                <input
+                  type={type}
+                  id="senha-entrar"
+                  name="senha-entrar"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
 
-            <div className="senha-field ct-input">
-              <label htmlFor="senha-entrar">Repeat Password</label>
-              <span onClick={handleToggle}>
-                <Icon icon={icon} size={20} />
-              </span>
-              <input
-                type={type}
-                id="senha-entrar"
-                name="senha-entrar"
-                placeholder="Repeat Password"
-                onChange={(e) => setRepeatPassword(e.target.value)}
-              />
-            </div>
-            <div className="remember-create-link">
-              <Link to="/" className="create-link">
-                I already have an account.
-              </Link>
-            </div>
-            <button type="submit" className="submit">
-              Register
-            </button>
-          </form>
-        </div>
-      </section>
-      ;
-    </div>
+              <div className="senha-field ct-input">
+                <label htmlFor="senha-entrar">Repeat Password</label>
+                <span onClick={handleToggle}>
+                  <Icon icon={icon} size={20} />
+                </span>
+                <input
+                  type={type}
+                  id="senha-entrar"
+                  name="senha-entrar"
+                  placeholder="Repeat Password"
+                  onChange={(e) => setRepeatPassword(e.target.value)}
+                />
+              </div>
+              <div className="remember-create-link">
+                <Link to="/" className="create-link">
+                  I already have an account.
+                </Link>
+              </div>
+              <button type="submit" className="submit">
+                Register
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+      <Snackbar open={openToast2} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Compra efetuada com Sucesso!
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
