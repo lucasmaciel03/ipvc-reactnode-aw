d56dc33e2f00b982e5ab3ac7f45757e6fc88955b
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-11 19:27:53 +0000

    verify if user as login

--- frontend/src/components/layout/products/Products.js
+++ frontend/src/components/layout/products/Products.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "../../filter/Filter";
 import classes from "./Products.module.css";
 import ProductsList from "./ProductsList";
+import jwtDecode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
 
 const Products = () => {
+  const [info, setUserInfo] = useState({});
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const hasToken = localStorage.getItem("token");
+    if (hasToken) {
+      const info = jwtDecode(hasToken);
+      setUserInfo(info);
+      // console.log(info.idUser);
+    } else {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="container">
       <main className={classes.main}>
