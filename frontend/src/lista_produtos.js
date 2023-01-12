66409c0a3e239d871d4e5fa0813c450a084c5789
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-12 16:45:38 +0000

    edit lista_produtos

--- frontend/src/lista_produtos.js
+++ frontend/src/lista_produtos.js
@@ -3,72 +3,9 @@ export const all_products = [
     name: "Avatar - O caminho da água",
     price: "6.49",
     img: "avatar.png",
-    category: "filmes",
+    category: "Ação",
     description:
       "Avatar - O Caminho da Água decorre uma década após os acontecimentos do primeiro filme e conta a história da família Sully (Jake, Neytiri e os seus filhos), os seus problemas, as longas distâncias que vão percorrer para se manterem a salvo, as batalhas que lutam para se manterem vivos, e as tragédias que suportam.",
   },
-  {
-    name: "Pantera Negra",
-    price: "5",
-    img: "blackpanther.jpeg",
-    category: "filmes",
-    description:
-      "Em Pantera Negra, após a morte do rei T'Chaka (John Kani), o príncipe T'Challa (Chadwick Boseman) retorna a Wakanda para a cerimônia de coroação. Nela são reunidas as cinco tribos que compõem o reino, sendo que uma delas, os Jabari, não apoia o atual governo. T'Challa logo recebe o apoio de Okoye (Danai Gurira), a chefe da guarda de Wakanda, da irmã Shuri (Letitia Wright), que coordena a área tecnológica do reino, e também de Nakia (Lupita Nyong'o), a grande paixão do atual Pantera Negra, que não quer se tornar rainha. Juntos, eles estão à procura de Ulysses Klaue (Andy Serkis), que roubou de Wakanda um punhado de vibranium, alguns anos atrás.",
-  },
-  {
-    name: "O Colibri",
-    price: "3.99",
-    img: "colibri.jpeg",
-    category: "filmes",
-    description:
-      "A vida de um homem desde os anos 70 até um futuro próximo numa miríade de relacionamentos que cruzam a sua vida, com consequências mais ou menos devastadoras.",
-  },
-  {
-    name: "A outra face do mal",
-    price: "5.49",
-    img: "facedomal.jpeg",
-    category: "filmes",
-    description:
-      "Na sequência de um trágico acidente que rouba a vida a um dos seus gémeos, Rachel e o marido Anthony mudam-se para o outro lado do mundo com o filho sobrevivente, na esperança de construir uma nova vida.",
-  },
-  {
-    name: "Glass Onion",
-    price: "7",
-    img: "glassOnion.png",
-    category: "filmes",
-    description:
-      "O ricaço Miles Bron convida seus amigos excêntricos para sua ilha para jogar um jogo de detetive. Mas um assassinato real é cometido e as coisas saem do controle. Todos são suspeitos e a morte pode vir de onde se menos imagina.",
-  },
-  {
-    name: "M3gan",
-    price: "4.99",
-    img: "megan.png",
-    category: "filmes",
-    description:
-      "M3GAN é uma maravilha da inteligência artificial, uma boneca realista programada para ser a melhor amiga de uma criança. Uma robótica brilhante dá a sua jovem sobrinha um protótipo M3GAN, mas a máquina logo se torna violenta.",
-  },
-  {
-    name: "Missão de Resgate",
-    price: "3.99",
-    img: "missaoresgate.jpeg",
-    category: "filmes",
-    description:
-      "O piloto Brodie Torrance salva os seus passageiros ao fazer uma aterragem arriscada numa ilha devastada pela guerra. Quando os sobreviventes são feitos reféns por rebeldes perigosos, a única pessoa com quem Torrance pode contar é Louis Gaspare, um assassino que estava a ser transportado pelo FBI. Para salvar os passageiros, Torrance precisará da ajuda de Gaspare... e descobrirá que este é mais do que aparenta.",
-  },
-  {
-    name: "O incrivél maurice",
-    price: "6.99",
-    img: "oincrivelmaurice.png",
-    category: "filmes",
-    description:
-      "Ratos! Estão por todo o lado... Na caixa do pão, a dançar em cima das mesas, a roubar tartes, mesmo debaixo do nariz da cozinheira. Do que é que toda a cidade precisa? Um bom flautista para os levar dali para fora. É aqui que entra o Maurice, um gato cheio de lábia, com o esquema perfeito para fazer dinheiro. Já todos ouviram as histórias do flautista e da cidade cheia de ratos. Maurice encontrou o seu próprio flautista e uma praga de ratos que usa para enganar cidadãos ingénuos e receber muito dinheiro.",
-  },
-  {
-    name: "R.M.N",
-    price: "2.59",
-    img: "rmn.jpeg",
-    category: "filmes",
-    description:
-      "Uns dias antes do Natal, e depois de se ter despedido do seu trabalho na Alemanha, Matthias volta para a sua aldeia multiétnica na Transilvânia. Pretende envolver-se mais na educação do filho, Rudi, deixado há demasiado ao cuidado da mãe, Ana, e livrar o rapaz dos medos não resolvidos que se apoderaram dele. Está preocupado com o seu velho pai, Otto, e ansioso por ver a sua antiga amada, Csilla. Quando são contratados alguns funcionários novos para a pequena fábrica que Csilla gere, a paz na comunidade é perturbada, o medo apodera-se dos adultos e frustrações, conflitos e paixões emergem sob uma camada fina de compreensão e calma aparentes.",
-  },
 ];
+
